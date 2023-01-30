44f90f79c1e12689a0cf6403c14f6ac6e011cca0
SunightMC <sunightmc@lunatee.ml>
2023-01-30 20:05:29 +0100

    Revert "Cleaned up the JavaScript files"
    This reverts commit 1d3e696c0dd5dab533bbf654ac296f316d7a4965.

--- src/components/js/downloader.js
+++ src/components/js/downloader.js
@@ -1,27 +1,28 @@
-function download(url, callback, encoding) {
+function download(url, callback, encoding){
   var request = http.get(url, function(response) {
-    if (encoding) {
-      response.setEncoding(encoding);
-    }
-    const len = parseInt(response.headers['content-length'], 10);
-    let body = '';
-    let cur = 0;
-    const obj = document.getElementById('js-progress');
-    const total = len / 1048576; // 1048576 - bytes in  1Megabyte
+      if (encoding){
+          response.setEncoding(encoding);
+      }
+      var len = parseInt(response.headers['content-length'], 10);
+      var body = "";
+      var cur = 0;
+      var obj = document.getElementById('js-progress');
+      var total = len / 1048576; //1048576 - bytes in  1Megabyte
 
-    response.on('data', function(chunk) {
-      body += chunk;
-      cur += chunk.length;
-      obj.innerHTML = 'Downloading ' + (100.0 * cur / len).toFixed(2) + '% ' + (cur / 1048576).toFixed(2) + ' mb\r' + '.<br/> Total size: ' + total.toFixed(2) + ' mb';
-    });
+      response.on("data", function(chunk) {
+          body += chunk;
+          cur += chunk.length;
+          obj.innerHTML = "Downloading " + (100.0 * cur / len).toFixed(2) + "% " + (cur / 1048576).toFixed(2) + " mb\r" + ".<br/> Total size: " + total.toFixed(2) + " mb";
+      });
 
-    response.on('end', function() {
-      callback(body);
-      obj.innerHTML = 'Downloading complete';
-    });
+      response.on("end", function() {
+          callback(body);
+          obj.innerHTML = "Downloading complete";
+      });
 
-    request.on('error', function(e) {
-      console.log('Error: ' + e.message);
-    });
+      request.on("error", function(e){
+          console.log("Error: " + e.message);
+      });
+
   });
 };
--- src/components/js/flashingrpc.js
+++ src/components/js/flashingrpc.js
@@ -1,25 +1,25 @@
-const DiscordRPC = require('discord-rpc');
+const DiscordRPC = require("discord-rpc");
 
-const clientId = '1012098922652631062';
-const rpc = new DiscordRPC.Client({transport: 'ipc'});
+const clientId = "1012098922652631062";
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
 const startTimestamp = new Date();
 
 async function setActivity() {
   rpc.setActivity({
-    details: 'Installing AvdanOS...',
-    state: 'Making the drive bootable..',
+    details: "Installing AvdanOS...",
+    state: "Making the drive bootable..",
     startTimestamp,
-    largeImageKey: 'defaultpfp',
-    largeImageText: 'Installing AvdanOS',
+    largeImageKey: "defaultpfp",
+    largeImageText: "Installing AvdanOS",
     instance: false,
   });
 }
 
-rpc.on('ready', () => {
+rpc.on("ready", () => {
   setActivity();
   setInterval(() => {
     setActivity();
   }, 15000);
 });
 
-rpc.login({clientId}).catch(console.error);
+rpc.login({ clientId }).catch(console.error);
